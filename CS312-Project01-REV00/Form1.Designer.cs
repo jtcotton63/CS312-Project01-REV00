895737888238323bdbe447f5d3a60729526f6aa2
Joseph Cotton <jtcotton63@gmail.com>
2016-02-03 13:36:06 -0700

    Finished product

---
+++ CS312-Project01-REV00/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CS312_Project01_REV00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.kvalue = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.solve = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(123, 13);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(309, 20);
+            this.input.TabIndex = 0;
+            // 
+            // kvalue
+            // 
+            this.kvalue.Location = new System.Drawing.Point(123, 48);
+            this.kvalue.Name = "kvalue";
+            this.kvalue.Size = new System.Drawing.Size(309, 20);
+            this.kvalue.TabIndex = 1;
+            this.kvalue.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(123, 84);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(309, 93);
+            this.output.TabIndex = 2;
+            // 
+            // solve
+            // 
+            this.solve.Location = new System.Drawing.Point(405, 238);
+            this.solve.Name = "solve";
+            this.solve.Size = new System.Drawing.Size(75, 23);
+            this.solve.TabIndex = 3;
+            this.solve.Text = "Solve";
+            this.solve.UseVisualStyleBackColor = true;
+            this.solve.Click += new System.EventHandler(this.solve_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number to test";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number of tests to run";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 273);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.solve);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.kvalue);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Primality Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox kvalue;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button solve;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CS312-Project01-REV00/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CS312_Project01_REV00
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void solve_Click(object sender, EventArgs e)
+        {
+            ulong toBeTested = Convert.ToUInt64(input.Text);
+            int kval = Convert.ToInt32(kvalue.Text);
+
+            if (toBeTested == 0)
+            {
+                output.Text = "Not prime with 100% confidence";
+                return;
+            }
+
+            if (toBeTested == 1)
+            {
+                output.Text = "Prime with 100% confidence";
+                return;
+            }
+
+            int numTimesFailed = 0;
+            for (int i = 0; i < kval; i++)
+            {
+                bool isPrime = fermat(toBeTested);
+                if (!isPrime)
+                    numTimesFailed++;
+            }
+
+            if (numTimesFailed >= (double) kval)
+            {
+                output.Text = "Not prime NumTimesFailed: " + numTimesFailed;
+            }
+            else
+            {
+                double chanceOfError = (1 - (Math.Pow(.5, (double) kval)))*100;
+                output.Text = "Prime with " + chanceOfError + "% confidence\n" + "numTestsFailed: " + numTimesFailed;
+            }
+
+        }
+
+        /*
+         * Fermat's theorem is prime numbers meet the requirement that
+         * a^p-1=1 mod P, for every a s.t. 1 <= a <= p.
+         * 
+         * toBeTested is equivalent to p in fermat's theorem.
+         */
+        private bool fermat(ulong toBeTested)
+        {
+            ulong rand = randomBtwn(toBeTested);
+            ulong remainder = modExp(rand, toBeTested - 1, toBeTested);
+            if (remainder == 1)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        private ulong randomBtwn(ulong max)
+        {
+            Random r = new Random();
+            if (max > Int32.MaxValue)
+            {
+                return (ulong) r.Next(1, Int32.MaxValue);
+            }
+            else
+            {
+                return (ulong) r.Next(1,(int) max);
+            }
+        }
+
+        /*
+         * Performs modular exponentiation.
+         * Outputs x^y % n
+         */
+        private ulong modExp(ulong x, ulong y, ulong n)
+        {
+            try
+            {
+                if (y == 0)
+                    return 1;
+
+                ulong z = modExp(x, y / 2, n);
+                if (y % 2 == 0)
+                {
+                    return ((z % n) * z) % n;
+                }
+                else
+                {
+                    return ((((z % n) * z) % n) * x) % n;
+                }
+            }
+            catch (OverflowException e)
+            {
+                MessageBox.Show("OVERFLOW EXCEPTION");
+                return 0;
+            }
+
+        }
+    }
+}
